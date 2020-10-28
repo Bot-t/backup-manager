c9b240e499ad9f66b47c8cf60239540d7ddddfd6
Bot-t <tangb7420@gmail.com>
2020-10-28 21:49:23 +0800

    过滤打包

--- build.php
+++ build.php
@@ -2,7 +2,7 @@
 // 参数内容为生成文件路径 此例中则在当前目录生成example.phar打包程序
 $phar = new Phar(__DIR__ . '/dist/backup.phar');
 $phar->startBuffering();
-$phar->buildFromDirectory(__DIR__);
+$phar->buildFromDirectory(__DIR__, '/\.php$/');
 $phar->compressFiles(Phar::GZ);
 $phar->setStub($phar->createDefaultStub('main.php'));
 $phar->stopBuffering();
